e4c08d9e9cd3a7552f4a651e0470091842108d2d
Eshratjahan17 <eshratjahan701@gmail.com>
2022-09-25 20:26:37 +0600

    all users and jwt

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,12 +1,13 @@
 const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema=mongoose.Schema(
   
 {
   name:{  type:String,require:true },
-  email:{ type:String,require:true  },
+  email:{ type:String,require:true ,unique:true  },
   password:{ type:String,require:true  },
-  pic:{ type:String,require:true ,
+  pic:{ type:String,
   default:"https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png"
   },
 
@@ -16,6 +17,18 @@ const userSchema=mongoose.Schema(
 
 }
 );
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 const User= mongoose.model("User",userSchema);
 
 module.exports=User;
--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 var cors = require("cors");
 app.use(cors());
+
 const dotenv = require("dotenv");
 const port =process.env.PORT||5000
 
 const {chats}=require("./Data");
 const connectDB = require("./config/db");
+const userRoutes=require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+
 dotenv.config();
 connectDB();
+
+
 app.get("/", (req, res) => {
   res.send("Doing again chat application!");
 });
 
+app.use('/api/user',userRoutes);
 
-app.get("/api/chat",(req,res)=>{
-  res.send(chats);
-});
-app.get("/api/chat/:id",(req,res)=>{
-  const chat=chats.find((c)=>c._id === req.params.id);
-  res.send(chat);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
